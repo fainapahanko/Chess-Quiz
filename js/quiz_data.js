ce5525fba51f5c2de3a7a970b82d9425e27a774c
Faina Poganko <fainapoganko@MacBook-Air-Faina.local>
2019-10-18 15:48:29 +0200

    final version

---
+++ js/quiz_data.js
@@ -0,0 +1,26 @@
+const quiz = [
+    {
+        "question" : "1) who was Boris Spassky's opponent for the chess crown in Iceland",
+        "answer" : {
+            "a" : "Garry Kasparov",
+            "b" : "Vladimir Kramnik",
+            "c" : "Bobby Fisher",
+        }
+    },
+    {
+        "question" : "2) who was Boris Spassky's opponent for the chess crown in Iceland",
+        "answer" : {
+            "a" : "Garry Kasparov",
+            "b" : "Vladimir Kramnik",
+            "c" : "Bobby Fisher",
+        }
+    },
+    {
+        "question" : "3) who was Boris Spassky's opponent for the chess crown in Iceland",
+        "answer" : {
+            "a" : "Garry Kasparov",
+            "b" : "Vladimir Kramnik",
+            "c" : "Bobby Fisher",
+        }
+    }
+]
